bd2b93d86de199559afd47aee1348576eaad74d2
checkelmann <checkelmann@0b1c000e-9708-11de-9e26-af68eadc8346>
2009-09-02 13:25:39 +0000

    New Import

---
+++ trunk/actions/views/default/gifts/form.php
@@ -0,0 +1,72 @@
+<?php
+	/**
+	 * Elgg Gifts plugin
+	 * Send gifts to you friends
+	 * 
+	 * @package Gifts
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Christian Heckelmann
+	 * @copyright Christian Heckelmann
+	 * @link http://www.heckelmann.info
+	 */
+?>
+<div class="contentWrapper">
+<form action="<?php echo $vars['url']; ?>action/gifts/sendgift" method="post">
+
+<?php
+	$send_to = get_input('send_to');
+	// Already send_to?
+	if($send_to){
+	    			//get the user object  
+    	        $user = get_user($send_to);
+    	        
+    	        //draw it
+    			echo "<label>" . elgg_echo("gifts:friend") . ":</label><br/><div class=\"messages_single_icon\">" . elgg_view("profile/icon",array('entity' => $user, 'size' => 'tiny')) . $user->username;
+    			echo "</div><br class=\"clearfloat\" />";
+    			//set the hidden input field to the recipients guid
+    	        echo "<input type=\"hidden\" name=\"send_to\" value=\"{$send_to}\" />";
+    	        
+    			    
+	        }else{
+
+?> 
+ 
+    <p><label><?php echo elgg_echo('gifts:friend'); ?>: </label>
+    <select name='send_to'>
+    <?php 
+        echo "<option value=''></option>";
+        foreach($vars['friends'] as $friend){
+            echo "<option value='{$friend->guid}'>" . $friend->name . "</option>";                
+        }
+               
+    ?>
+    </select></p>
+    
+    <?php
+        // Add Friends Picker to send gifts to multiple firends
+        /*if ($friends = get_entities_from_relationship('friend',$_SESSION['guid'],false,'user','',0,'',9999)) {
+            echo elgg_view('friends/picker',array('entities' => $friends, 'internalname' => 'user_guid', 'highlight' => 'all'));    
+        }*/
+    ?>
+<?php
+} ?>
+<p><?php echo elgg_echo('gifts:selectgift'); ?><br />
+    <select name='gift_id'>
+<?php
+    $gift_count = get_plugin_setting('giftcount'.$i, 'gifts');
+    for ($i=1;$i<=$gift_count;$i++)
+    {
+        echo "<option value='{$i}'>".get_plugin_setting('gift_'.$i, 'gifts')."</option>";
+    }    
+?>
+    </select>
+</p>
+ 
+<p><input type="submit" value="<?php echo elgg_echo('gifts:send'); ?>" /></p>
+
+<p><div id="gift_preview">
+<!-- Placeholder for Giftimage -->
+</div></p>
+ 
+</form>
+</div>
