131e08479f49095673fe3f0351aa66e42d25027c
checkelmann <checkelmann@0b1c000e-9708-11de-9e26-af68eadc8346>
2009-09-06 12:19:10 +0000

    checkelmann

---
+++ trunk/languages/de.php
@@ -0,0 +1,52 @@
+<?php
+
+	/**
+	 * Elgg Gifts plugin
+	 * Send gifts to you friends
+	 * 
+	 * @package Gifts
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Christian Heckelmann
+	 * @copyright Christian Heckelmann
+	 * @link http://www.heckelmann.info
+	 */
+
+
+$german = array( 
+	 'gifts:menu'  =>  "Geschenke" , 
+	 'gifts:yourgifts'  =>  "Meine Geschenke",
+     'gifts:allgifts'  =>  "Alle Geschenke",
+	 'gifts:sent'  =>  "Geschickte Geschenke",
+     'gifts:sendgifts' => "Schenke etwas",
+     'gifts:friend' => "Freund",
+	 'gifts:message' => "Nachricht",
+     'gifts:selectgift' => "W&auml;hle ein Geschenk",
+     'gifts:gift' => "Geschenk",
+     'gifts:send' => "Sende Geschenk",     
+     'gifts:object' => "%s erhielt %s von %s",
+     'gifts:river' => "%s erhielt ein %s von ",
+     'gifts:blank' => "Du mu&szlig;t alle Felder ausf&uuml;llen!",
+     'item:object:gift' => 'Geschenke',
+     'gifts:settings:number' => "Anzahl der Geschenke",
+     'gifts:settings:title' =>  "Geschenk",
+     'gifts:settings:globalsettings' =>  "Einstellungen",
+     'gifts:settings:giftsettings' =>  "Geschenke",
+'gifts:settings:useuserpoints' =>  "Userpoints verwenden",
+'gifts:settings:userpoints' =>  "Punkte",
+	 'gifts:settings:code' =>  "Giftcode",
+	 'gifts:settings:showallyes' =>  "Ja",
+	 'gifts:settings:showallno' =>  "Nein",
+	 'gifts:settings:showallgifts' =>  "Alle Geschenke Anzeigen",
+	 'gifts:settings:saveok' =>  "Einstellungen erfolgreich gespeichert",
+	 'gifts:settings:savefail' =>  "Konnte Einstellungen nicht speichern!",
+	 'gifts:mail:subject' => "Du hast ein neues Geschenk erhalten!",
+	 'gifts:mail:body' => "Du hast ein neues Geschenk von %s. 
+     
+Du findest dein Geschenk hier: %s 
+
+Du kannst auf diese Mail nicht Antworten"
+); 
+
+add_translation('de', $german); 
+
+?>
